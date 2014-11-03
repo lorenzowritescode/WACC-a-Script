69bd070a825e39b3f10a96fc3c9623fb113c37ed
Lorenzo Paoliani <lorenzo.paoliani13@imperial.ac.uk>
2014-11-03 13:09:15 +0000

    Created skeleton for compiler

---
+++ src/SemanticChecker.java
@@ -0,0 +1,13 @@
+package antlr;
+
+import org.antlr.v4.runtime.tree.ParseTree;
+
+public class SemanticChecker {
+	
+	private ParseTree waccTree;
+
+	public SemanticChecker(ParseTree t){
+		this.waccTree = t;
+	}
+
+}
---
+++ src/WACCCompiler.java
@@ -0,0 +1,35 @@
+package antlr;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+public class WACCCompiler {
+
+	public static void main(String[] args) throws IOException {
+		InputStream waccInput;
+		if(args.length > 0)
+			waccInput = new FileInputStream(args[0]);
+		else
+			waccInput = System.in;
+		ANTLRInputStream antrlInput = new ANTLRInputStream(waccInput);
+		WACCLexer lexer = new WACCLexer(antrlInput);
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		WACCParser parser = new WACCParser(tokens);
+		ParseTree tree = parser.prog();
+		
+		SemanticChecker semantic = new SemanticChecker(tree);
+		WACCCompiler compiler = new WACCCompiler(semantic);
+	}
+
+	private SemanticChecker semanticChecker;
+	
+	public WACCCompiler(SemanticChecker s){
+		this.semanticChecker = s;
+	}
+
+}
