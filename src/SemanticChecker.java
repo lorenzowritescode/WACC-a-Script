3ce8a496930c570162f85b6918f3df11c5fd4438
or813 <evenoli@dyn1205-147.wlan.ic.ac.uk>
2014-11-03 18:38:20 +0000

    Merging

--- src/SemanticChecker.java
+++ src/SemanticChecker.java
@@ -1,14 +1,53 @@
-import org.antlr.v4.runtime.tree.ParseTree;
-package antlr;
 
+
+import java.util.Map;
+
+import org.antlr.v4.misc.OrderedHashMap;
 import org.antlr.v4.runtime.tree.ParseTree;
 
-public class SemanticChecker {
+import WACCParser.Pair_literContext;
+
+
+public class SemanticChecker extends WACCParserBaseVisitor<ParseTree> {
 	
-	private ParseTree waccTree;
+	@Override
+	public ParseTree visitPair_liter(Pair_literContext ctx) {
+		// TODO Auto-generated method stub
+		return super.visitPair_liter(ctx);
+	}
 
+	@Override
+	public ParseTree visitStr_liter(Str_literContext ctx) {
+		// TODO Auto-generated method stub
+		return super.visitStr_liter(ctx);
+	}
+
+	@Override
+	public ParseTree visitBool_liter(Bool_literContext ctx) {
+		// TODO Auto-generated method stub
+		return super.visitBool_liter(ctx);
+	}
+
+	@Override
+	public ParseTree visitArray_liter(Array_literContext ctx) {
+		// TODO Auto-generated method stub
+		return super.visitArray_liter(ctx);
+	}
+
+	@Override
+	public ParseTree visitInt_liter(Int_literContext ctx) {
+		// TODO Auto-generated method stub
+		return super.visitInt_liter(ctx);
+	}
+
+	private ParseTree waccTree;
+	private Map<String, varType> varMap = new OrderedHashMap<String, varType>();
+	
 	public SemanticChecker(ParseTree t){
 		this.waccTree = t;
 	}
+	
+	
+	
 
 }
