2cc35577aeefefbd9d93444cb6f03db1e921c6b1
Lorenzo Paoliani <lorenzo.paoliani13@imperial.ac.uk>
2014-11-06 14:28:20 +0000

    Minor changes

--- src/compiler/antlr/SemanticChecker.java
+++ src/compiler/antlr/SemanticChecker.java
@@ -2,12 +2,20 @@ package antlr;
 
 import org.antlr.v4.runtime.tree.ParseTree;
 
-public class SemanticChecker {
+import antlr.WACCParser.FuncContext;
+import antlr.WACCParser.Variable_assigmentContext;
+import antlr.WACCParser.Variable_declarationContext;
+
+public class SemanticChecker extends WACCParserBaseVisitor<Void>{
 	
 	private ParseTree waccTree;
-
+	
 	public SemanticChecker(ParseTree t){
 		this.waccTree = t;
+	}
+	
+	public void init() {
+		waccTree.accept(this);
 	}
 
 }
