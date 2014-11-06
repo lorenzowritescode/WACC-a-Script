6bcd52114bdbd48ced829aa470a00b601fdcb669
Lorenzo Paoliani <lorenzo.paoliani13@imperial.ac.uk>
2014-11-06 16:11:45 +0000

    Created custom Exception

---
+++ src/compiler/WACCExceptions/NotUniqueIdentifierException.java
@@ -0,0 +1,14 @@
+package WACCExceptions;
+
+public class NotUniqueIdentifierException extends RuntimeException {
+
+	public NotUniqueIdentifierException(String exceptionMessage) {
+		super(exceptionMessage);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5645623132976907968L;
+
+}
