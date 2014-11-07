ac82db661638e81c207862eb1cadcc739b9a5444
Lorenzo Paoliani <lorenzo.paoliani13@imperial.ac.uk>
2014-11-07 10:11:45 +0000

    Added enum type for WACC types

---
+++ src/compiler/antlr/WACCType.java
@@ -0,0 +1,24 @@
+package antlr;
+
+import WACCExceptions.InvalidTypeException;
+import antlr.WACCParser.TypeContext;
+
+public enum WACCType {
+	INT, BOOL, CHAR, STRING;
+	
+	public WACCType evalType(TypeContext ctx){
+		String type = ctx.getText();
+		switch (type) {
+		case "INT":
+			return INT;
+		case "BOOL":
+			return BOOL;
+		case "CHAR":
+			return CHAR;
+		case "STRING":
+			return STRING;
+		default:
+			throw new InvalidTypeException("The type provided was not recognised.", ctx);
+		}
+	}
+}
