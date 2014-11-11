0df972709b976bbd3b70f0d02f3b5ea5b38bff23
or813 <evenoli@dyn1205-147.wlan.ic.ac.uk>
2014-11-11 12:04:49 +0000

    Added Exit and free statement nodes, and an abstract expression node

---
+++ src/compiler/tree/ExprNode.java
@@ -0,0 +1,19 @@
+package tree;
+
+import symboltable.SymbolTable;
+
+public abstract class ExprNode extends WACCTree {
+
+	@Override
+	public boolean check(SymbolTable st) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public WACCType getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
