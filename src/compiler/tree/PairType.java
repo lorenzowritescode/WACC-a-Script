39c3ce9e224939830cacf860769ca8b13ceca9a3
or813 <evenoli@dyn1205-147.wlan.ic.ac.uk>
2014-11-11 12:41:53 +0000

    Implemented pair Type

---
+++ src/compiler/tree/PairType.java
@@ -0,0 +1,33 @@
+package tree;
+
+public class PairType implements WACCType {
+
+	private WACCType fst;
+	private WACCType snd;
+
+	public PairType(WACCType fst, WACCType snd) {
+		this.fst = fst;
+		this.snd = snd;
+	}
+
+	public WACCType getFst() {
+		return fst;
+	}
+
+	public WACCType getSnd() {
+		return snd;
+	}
+
+	@Override
+	public boolean isCompatible(WACCType other) {
+		if (!(other instanceof PairType)) {
+			return false;
+		}
+		PairType otherPair = (PairType) other;
+		if (!fst.isCompatible(otherPair.getFst()) && !snd.isCompatible(otherPair.getSnd())) {
+			return false;
+		}
+		return true;
+	}
+
+}
