63b00568945622d84a408e50d2751f80596cdb9a
or813 <evenoli@dyn1205-147.wlan.ic.ac.uk>
2014-11-11 12:42:29 +0000

    Changed WACCType to an interface

--- src/compiler/tree/WACCType.java
+++ src/compiler/tree/WACCType.java
@@ -1,25 +1,8 @@
 package tree;
 
-import WACCExceptions.InvalidTypeException;
-import antlr.WACCParser;
-import antlr.WACCParser.TypeContext;
 
-public enum WACCType {
-	INT, BOOL, CHAR, STRING;
+public interface WACCType {
+		
+	public boolean isCompatible(WACCType other);
 	
-	public static WACCType evalType(TypeContext ctx){
-		String type = ctx.getText();
-		switch (type) {
-		case "int":
-			return INT;
-		case "bool":
-			return BOOL;
-		case "char":
-			return CHAR;
-		case "string":
-			return STRING;
-		default:
-			throw new InvalidTypeException("The type provided was not recognised.", ctx);
-		}
-	}
 }
