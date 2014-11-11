69bcd76b2b773732eeceea8467ab2ff35c87dcec
Sam Budd <samuel.budd13@imperial.ac.uk>
2014-11-11 15:22:04 +0000

    IfStat and WhileSat partially implemented

--- src/compiler/tree/stat/IfStatNode.java
+++ src/compiler/tree/stat/IfStatNode.java
@@ -1,5 +1,27 @@
 package tree.stat;
 
+import WACCExceptions.InvalidTypeException;
+import symboltable.SymbolTable;
+import tree.ExprNode;
+import tree.type.WACCType;
+
 public class IfStatNode extends StatNode {
+	
+	private ExprNode expr;
+	
+	public IfStatNode(ExprNode expr) {
+		this.expr = expr;
+	}
+	
+	@Override
+	public boolean check(SymbolTable st) {
+		if (expr.getType() == WACCType.BOOL) {
+			return true;
+		} else {
+			el.record(new InvalidTypeException("If statements should have an expr of type BOOL", ));
+		    return false;
+		}
+	}
+	
 
 }
--- src/compiler/tree/stat/WhileStatNode.java
+++ src/compiler/tree/stat/WhileStatNode.java
@@ -1,5 +1,25 @@
 package tree.stat;
 
+import symboltable.SymbolTable;
+import tree.ExprNode;
+import tree.type.WACCType;
+
 public class WhileStatNode extends StatNode {
+	
+	private ExprNode expr;
+	
+	public WhileStatNode(ExprNode expr) {
+		this.expr = expr;
+	}
+	
+	@Override
+	public boolean check(SymbolTable st) {
+		if (expr.getType() == WACCType.BOOL) {
+			return true;
+		} else {
+			el.record(new InvalidTypeException("While statement should have an expr of type BOOL", ));
+			return false;
+		}
+	}
 
 }
