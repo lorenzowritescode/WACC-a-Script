eb44a2250c097064a44e4f5459c1b4e6c4473ab9
or813 <evenoli@dyn1205-147.wlan.ic.ac.uk>
2014-11-12 12:35:50 +0000

    ExprNode now extends assign-rhs for assignment statements

--- src/compiler/tree/expr/ExprNode.java
+++ src/compiler/tree/expr/ExprNode.java
@@ -2,11 +2,12 @@ package tree.expr;
 
 import org.antlr.v4.runtime.RuleContext;
 
+import assignments.AssignRhsNode;
 import symboltable.SymbolTable;
 import tree.WACCTree;
 import tree.type.WACCType;
 
-public abstract class ExprNode extends WACCTree {
+public abstract class ExprNode extends AssignRhsNode  {
 
 	//Abstract class for all expressions to extend
 	
