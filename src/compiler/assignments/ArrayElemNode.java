a3d4dd1ce942d385eb60a4680c352ad7e92f1005
or813 <evenoli@dyn1205-147.wlan.ic.ac.uk>
2014-11-12 12:53:43 +0000

    Created ArrayElemNode

---
+++ src/compiler/assignments/ArrayElemNode.java
@@ -0,0 +1,41 @@
+package assignments;
+
+import org.antlr.v4.runtime.RuleContext;
+
+import symboltable.SymbolTable;
+import tree.expr.ExprNode;
+import tree.type.WACCType;
+import WACCExceptions.InvalidTypeException;
+
+public class ArrayElemNode extends AssignLhsNode {
+	
+	String ident;
+	ExprNode pos;
+	WACCType arrayType;
+
+	public ArrayElemNode(String ident, ExprNode expr, WACCType type) {
+		this.ident = ident;
+		this.pos = expr;
+		this.arrayType = type;
+	}
+	
+	@Override
+	public boolean check( SymbolTable st, RuleContext ctx ) {
+		if(!(pos.getType() == WACCType.INT)) {
+			el.record(new InvalidTypeException("Array position can only be found using an Int", ctx));
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public WACCType getType() {
+		return arrayType;
+	}
+	
+	@Override
+	public String getIdent() {
+		return ident;
+	}
+
+}
