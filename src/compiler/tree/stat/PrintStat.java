e502ebb56effe3d3ef3c590724396d1376f4454c
or813 <evenoli@dyn1205-147.wlan.ic.ac.uk>
2014-11-12 12:29:54 +0000

    Updated print statements to take an expression

--- src/compiler/tree/stat/PrintStat.java
+++ src/compiler/tree/stat/PrintStat.java
@@ -1,4 +1,12 @@
 package tree.stat;
 
+import tree.expr.ExprNode;
+
 public class PrintStat extends StatNode {
+	
+	private ExprNode expr;
+	
+	public PrintStat(ExprNode expr) {
+		this.expr = expr;
+	}
 }
