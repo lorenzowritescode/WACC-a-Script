20fb67bc0252d205134fc6eecd71080f1538d22d
Lorenzo Paoliani <lorenzo.paoliani13@imperial.ac.uk>
2014-11-13 10:19:22 +0000

    Implementation of ExprNode should be empty and abstract

--- src/compiler/tree/expr/ExprNode.java
+++ src/compiler/tree/expr/ExprNode.java
@@ -1,23 +1,9 @@
 package tree.expr;
 
-import org.antlr.v4.runtime.RuleContext;
+import tree.WACCTree;
 
-import symboltable.SymbolTable;
-import tree.type.WACCType;
-import assignments.AssignRhsNode;
-
-public abstract class ExprNode extends AssignRhsNode  {
-
-	//Abstract class for all expressions to extend
-	
-	@Override
-	public boolean check(SymbolTable st, RuleContext ctx) {
-		return false;
-	}
-
-	@Override
-	public WACCType getType() {
-		return null;
-	}
-
+public abstract class ExprNode extends WACCTree  {
+	/*
+	 * The implementation of this class is left empty and abstract, and will be used as a super class for concrete expr classes.
+	 */
 }
