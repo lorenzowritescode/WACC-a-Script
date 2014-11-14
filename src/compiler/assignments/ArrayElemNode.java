ad23ab3faf3a86c79d8900bc0be8afce5b13fac5
or813 <evenoli@dyn1205-147.wlan.ic.ac.uk>
2014-11-14 17:42:13 +0000

    Fixed implementation of ArrayElemNode

--- src/compiler/assignments/ArrayElemNode.java
+++ src/compiler/assignments/ArrayElemNode.java
@@ -1,5 +1,7 @@
 package assignments;
 
+import java.util.ArrayList;
+
 import org.antlr.v4.runtime.ParserRuleContext;
 
 import symboltable.SymbolTable;
@@ -10,20 +12,22 @@ import WACCExceptions.InvalidTypeException;
 public class ArrayElemNode extends ExprNode {
 	
 	String ident;
-	ExprNode pos;
+	ArrayList<ExprNode> locations;
 	WACCType arrayType;
 
-	public ArrayElemNode(String ident, ExprNode expr, WACCType type) {
+	public ArrayElemNode(String ident, ArrayList<ExprNode> locations, WACCType type) {
 		this.ident = ident;
-		this.pos = expr;
+		this.locations = locations;
 		this.arrayType = type;
 	}
 	
 	@Override
 	public boolean check( SymbolTable st, ParserRuleContext ctx ) {
-		if(!(pos.getType() == WACCType.INT)) {
-			el.record(new InvalidTypeException("Array position can only be found using an Int", ctx));
-			return false;
+		for(ExprNode pos : locations) {
+			if(!(pos.getType() == WACCType.INT)) {
+				el.record(new InvalidTypeException("Array position can only be found using an Int", ctx));
+				return false;
+			}
 		}
 		return true;
 	}
