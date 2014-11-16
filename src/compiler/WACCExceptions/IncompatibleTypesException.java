454210eae3d33256725072d0f7a1c3d10f0fa535
Lorenzo Paoliani <lorenzo.paoliani13@imperial.ac.uk>
2014-11-16 18:05:52 +0000

    Added String only constructor to these exceptions

--- src/compiler/WACCExceptions/IncompatibleTypesException.java
+++ src/compiler/WACCExceptions/IncompatibleTypesException.java
@@ -2,15 +2,15 @@ package WACCExceptions;
 
 import org.antlr.v4.runtime.ParserRuleContext;
 
+@SuppressWarnings("serial")
 public class IncompatibleTypesException extends WACCException {
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 554801317748591605L;
-
+	
 	public IncompatibleTypesException(String exceptionMessage, ParserRuleContext ctx) {
 		super(exceptionMessage, ctx);
+	}
+
+	public IncompatibleTypesException(String message) {
+		super(message);
 	}
 
 }
