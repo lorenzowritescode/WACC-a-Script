aff2e5010b49e0554b5c435513d9893b99c78e3d
or813 <evenoli@dyn1205-147.wlan.ic.ac.uk>
2014-11-16 17:50:31 +0000

    updated check method of freeStat

--- src/compiler/tree/stat/FreeStat.java
+++ src/compiler/tree/stat/FreeStat.java
@@ -5,6 +5,8 @@ import org.antlr.v4.runtime.ParserRuleContext;
 import symboltable.SymbolTable;
 import tree.expr.ExprNode;
 import tree.expr.IdentNode;
+import tree.type.ArrayTypeNode;
+import tree.type.PairTypeNode;
 import WACCExceptions.InvalidTypeException;
 
 public class FreeStat extends StatNode {
@@ -15,12 +17,26 @@ public class FreeStat extends StatNode {
 		this.en = en;
 	}
 	
+	/* (non-Javadoc)
+	 * @see tree.stat.StatNode#check(symboltable.SymbolTable, org.antlr.v4.runtime.ParserRuleContext)
+	 * Checks to see if expr is an identifier, and if it is,
+	 * checks that if that the identifier is in symbol table, it is a pair or an array type
+	 */
 	public boolean check( SymbolTable st, ParserRuleContext ctx ) {
-		if (!(en instanceof IdentNode)) {
-			new InvalidTypeException("'Free' must be passed an identifier to a variable", ctx);
-			return false;
+		if (en instanceof IdentNode) {
+			IdentNode identN = (IdentNode) en;
+			String ident = identN.getIdent();
+			if (st.containsRecursive(ident)) {
+				if (!(st.get(ident).getType() instanceof ArrayTypeNode 
+					|| st.get(ident).getType() instanceof PairTypeNode)) {
+					new InvalidTypeException("Can only free an Array or a Pair", ctx);
+					return false;
+				}
+			}
+			return true;
 		}
-		return true;
+		new InvalidTypeException("'Free' must be passed an identifier to a variable", ctx);
+		return false;
 	}
 
 }
