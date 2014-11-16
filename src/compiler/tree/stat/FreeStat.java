8757ad4bf3bceabd86f6ece4a81bb87541671d1d
or813 <evenoli@dyn1205-147.wlan.ic.ac.uk>
2014-11-16 17:49:37 +0000

    Added check method to freeStat

--- src/compiler/tree/stat/FreeStat.java
+++ src/compiler/tree/stat/FreeStat.java
@@ -1,6 +1,11 @@
 package tree.stat;
 
+import org.antlr.v4.runtime.ParserRuleContext;
+
+import symboltable.SymbolTable;
 import tree.expr.ExprNode;
+import tree.expr.IdentNode;
+import WACCExceptions.InvalidTypeException;
 
 public class FreeStat extends StatNode {
 	
@@ -8,6 +13,14 @@ public class FreeStat extends StatNode {
 	
 	public FreeStat(ExprNode en) {
 		this.en = en;
+	}
+	
+	public boolean check( SymbolTable st, ParserRuleContext ctx ) {
+		if (!(en instanceof IdentNode)) {
+			new InvalidTypeException("'Free' must be passed an identifier to a variable", ctx);
+			return false;
+		}
+		return true;
 	}
 
 }
