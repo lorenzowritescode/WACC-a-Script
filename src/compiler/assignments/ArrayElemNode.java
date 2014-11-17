97ddb28cc8c2b6bc23d2727879b0e571471f2631
Sam Budd <samuel.budd13@imperial.ac.uk>
2014-11-17 10:24:53 +0000

    ArrayElemNode modified

--- src/compiler/assignments/ArrayElemNode.java
+++ src/compiler/assignments/ArrayElemNode.java
@@ -36,7 +36,11 @@ public class ArrayElemNode extends ExprNode implements AssignLhsNode {
 
 	@Override
 	public WACCType getType() {
-		return arrayType;
+		if (arrayType == WACCType.STRING) {
+			return WACCType.CHAR;
+		} else {
+			return arrayType;
+		}
 	}
 	
 	
