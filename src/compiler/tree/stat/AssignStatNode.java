44adcdc61508fd76d02bee95d2cda2ff2a629621
Sam Budd <samuel.budd13@imperial.ac.uk>
2014-11-17 13:16:12 +0000

    modifyString.wacc error resolved

--- src/compiler/tree/stat/AssignStatNode.java
+++ src/compiler/tree/stat/AssignStatNode.java
@@ -2,6 +2,7 @@ package tree.stat;
 
 import org.antlr.v4.runtime.ParserRuleContext;
 
+import assignments.ArrayElemNode;
 import assignments.AssignLhsNode;
 import assignments.Assignable;
 import symboltable.SymbolTable;
@@ -37,7 +38,9 @@ public class AssignStatNode extends StatNode {
 		
 		//TODO: will this work with arrays? or will it assign the entire 
 		// array to equal the rhs node?
-		st.update(lhs.getIdent(), rhs);
+		if (!(lhs instanceof ArrayElemNode)) {
+			st.update(lhs.getIdent(), rhs);
+		}
 		return true;
 		
 	}
