d83b37f3aeb5ab72c1dd1ef2ca5ff65b46d85b1e
Sam Budd <samuel.budd13@imperial.ac.uk>
2014-11-17 16:25:14 +0000

    Comments for type files

--- src/compiler/tree/type/PairType.java
+++ src/compiler/tree/type/PairType.java
@@ -3,6 +3,12 @@ package tree.type;
 import WACCExceptions.IncompatibleTypesException;
 
 public class PairType extends WACCType {
+	
+	/* Class for the Pair Type
+	 * Contains type info for 1st and 2nd elements and functionality for NULLs
+	 * Functionality to check other types are compatible with the pair
+	 */
+	
 	private static PairType NULL_PAIR = new PairType(WACCType.NULL, WACCType.NULL);
 	private WACCType fst;
 	private WACCType snd;
--- src/compiler/tree/type/WACCArithBinOp.java
+++ src/compiler/tree/type/WACCArithBinOp.java
@@ -4,6 +4,10 @@ import tree.expr.ExprNode;
 
 public class WACCArithBinOp extends WACCBinOp {
 	
+	/* Contains specific behavior for Arithmetic Binary Expressions
+	 * See WACCBinOp.java for more 
+	 */
+	
 	@Override
 	public boolean check(ExprNode lhs, ExprNode rhs) {
 		return lhs.getType() == rhs.getType() && lhs.getType() == WACCType.INT;
