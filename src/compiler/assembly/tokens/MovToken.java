5bd007a73d4626ffc02f8c9a9aaa0aadb4366613
or813 <evenoli@dyn1205-147.wlan.ic.ac.uk>
2014-11-21 13:15:31 +0000

    Changed movToken to take a condition argument, and include it with toString

--- src/compiler/assembly/tokens/MovToken.java
+++ src/compiler/assembly/tokens/MovToken.java
@@ -1,24 +1,40 @@
 package assembly.tokens;
 
+import assembly.InstrToken;
 import assembly.Register;
 import tree.expr.IntLeaf;
 
-public class MovToken {
+public class MovToken extends InstrToken{
 	private Register dest;
 	private Object source;
-
+	private String condition;
+	
 	public MovToken(Register r, IntLeaf n) {
 		this.dest = r;
 		this.source = n;
+		this.condition = "";
 	}
 	
 	public MovToken(Register rDest, Register rSrc){
 		this.dest = rDest;
 		this.source = rSrc;
+		this.condition = "";
+	}
+
+	public MovToken(String condition, Register r, IntLeaf n) {
+		this.dest = r;
+		this.source = n;
+		this.condition = condition;
+	}
+	
+	public MovToken(String condition, Register rDest, Register rSrc){
+		this.dest = rDest;
+		this.source = rSrc;
+		this.condition = condition;
 	}
 	
 	@Override
 	public String toString() {
-		return "MOV " + dest.toString() + ", " + source.toString();
+		return "MOV" + condition + " " + dest.toString() + ", " + source.toString();
 	}
 }
