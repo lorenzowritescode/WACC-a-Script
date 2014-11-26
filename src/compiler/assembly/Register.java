bb89db7044dd72f50791750b2e754e5fa43a62c2
or813 <evenoli@dyn1205-147.wlan.ic.ac.uk>
2014-11-26 14:42:05 +0000

    Implemented basic register class, and a register allocator class which manages and allocates unused registers

--- src/compiler/assembly/Register.java
+++ src/compiler/assembly/Register.java
@@ -1,5 +1,16 @@
 package assembly;
 
 public class Register {
+	
+	private String regName;
+	
+	public Register(String regName) {
+		this.regName = regName;
+	}
+	
+	@Override
+	public String toString() {
+		return regName;
+	}
 
 }
--- src/compiler/assembly/RegisterAllocator.java
+++ src/compiler/assembly/RegisterAllocator.java
@@ -1,5 +1,61 @@
 package assembly;
 
-public class RegisterAllocator {
+import java.util.ArrayList;
 
+public class RegisterAllocator {
+	
+	private ArrayList<Register> freeRegs;
+	private ArrayList<Register> argRegs;
+	
+	/**
+	 * Fills register lists with all possible registers (i.e. sets all registers to free)
+	 */
+	public void init() {
+		freeRegs.clear();
+		argRegs.clear();
+		for (int i = 4; i < 13; i++) {
+			Register reg = new Register("R" + i);
+			freeRegs.add(reg);
+		}
+		for (int i = 0; i < 4; i++) {
+			Register reg = new Register("R" + i);
+			argRegs.add(reg);
+		}
+	}
+	
+	
+	/**
+	 * @return returns next free general register
+	 */
+	public Register getGeneralReg() {
+		return freeRegs.remove(0);
+	}
+	
+	
+	/**
+	 * clears and resets free general register list
+	 */
+	public void resetGenralRegs() {
+		freeRegs.clear();
+		for (int i = 4; i < 13; i++) {
+			Register reg = new Register("R" + i);
+			freeRegs.add(reg);
+		}
+	}
+	
+	/**
+	 * @return returns next free argument register
+	 */
+	public Register getArgReg() {
+		return argRegs.remove(0);
+	}
+	
+	public void resetArgRegs() {
+		argRegs.clear();
+		for (int i = 0; i < 4; i++) {
+			Register reg = new Register("R" + i);
+			argRegs.add(reg);
+		}
+	}
+	
 }
