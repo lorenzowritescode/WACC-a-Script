7686f2a8da68b931b47c429960ca543f434a1d28
Lorenzo Paoliani <lorenzo.paoliani13@imperial.ac.uk>
2014-11-27 16:18:02 +0000

    Implemented getNext() and created static registers R0, R1, R2, R3

--- src/compiler/assembly/Register.java
+++ src/compiler/assembly/Register.java
@@ -2,19 +2,22 @@ package assembly;
 
 public class Register {
 	
-	private String varName;
 	private Integer registerNumber;
+	private Register next;
 	
 	public Register(String varName) {
-		this.varName = varName;
+		this.next = new Register();
+	}
+	public Register() {
+		this.next = new Register();
 	}
 	
 	public void setRegister(int reg) {
 		this.registerNumber = reg;
 	}
 	
-	public Register() {
-		varName = null;
+	public Register getNext() {
+		return next;
 	}
 	
 	@Override
@@ -25,5 +28,51 @@ public class Register {
 		
 		return "r" + registerNumber;
 	}
+	
 
+	public static final Register R0 = new Register() {
+		@Override
+		public String toString() {
+			return "r0";
+		}
+		
+		@Override
+		public Register getNext() {
+			return R1;
+		}
+	};
+	public static final Register R1 = new Register() {
+		@Override
+		public String toString() {
+			return "r1";
+		}
+		
+		@Override
+		public Register getNext() {
+			return R2;
+		}
+	};
+	public static final Register R2 = new Register() {
+		@Override
+		public String toString() {
+			return "r2";
+		}
+		
+		@Override
+		public Register getNext() {
+			return R3;
+		}
+	};
+
+	public static final Register R3 = new Register() {
+		@Override
+		public String toString() {
+			return "r3";
+		}
+		
+		@Override
+		public Register getNext() {
+			throw new UnsupportedOperationException("Can't request a register after R3");
+		}
+	};
 }
