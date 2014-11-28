0ec493713e8176251ddb66985dce1ac2d4b50ffe
or813 <evenoli@dyn1205-147.wlan.ic.ac.uk>
2014-11-28 12:23:14 +0000

    fixed token error in toAssembly of ExitStat

--- src/compiler/tree/stat/ExitStat.java
+++ src/compiler/tree/stat/ExitStat.java
@@ -4,14 +4,13 @@ import org.antlr.v4.runtime.ParserRuleContext;
 
 import symboltable.SymbolTable;
 import tree.expr.ExprNode;
-import tree.expr.IntLeaf;
 import tree.type.WACCType;
 import WACCExceptions.InvalidTypeException;
 import assembly.InstrToken;
 import assembly.Register;
 import assembly.TokenSequence;
 import assembly.tokens.BranchLinkToken;
-import assembly.tokens.MovToken;
+import assembly.tokens.MovRegToken;
 
 /**
  * Class to represent exit statements for exiting a program
@@ -38,12 +37,12 @@ public class ExitStat extends StatNode {
 	
 	@Override
 	public TokenSequence toAssembly(Register dest) {
-		TokenSequence mov = exitVal.toAssembly(dest);
-		InstrToken mov2 = new MovToken(Register.R0, dest);
+		TokenSequence ldr = exitVal.toAssembly(dest);
+		InstrToken mov2 = new MovRegToken(Register.R0, dest);
 		InstrToken branch = new BranchLinkToken("exit");
 		
-		mov.appendAll(new TokenSequence(mov2, branch));
-		return mov;
+		ldr.appendAll(new TokenSequence(mov2, branch));
+		return ldr;
 	}
 
 }
