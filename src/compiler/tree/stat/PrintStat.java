c1a16263f3a847db473450253dbb68a137d97ebd
or813 <evenoli@dyn1205-147.wlan.ic.ac.uk>
2014-11-29 14:56:42 +0000

    Implemented toAssembly for printStat

--- src/compiler/tree/stat/PrintStat.java
+++ src/compiler/tree/stat/PrintStat.java
@@ -1,6 +1,10 @@
 package tree.stat;
 
 import tree.expr.ExprNode;
+import assembly.InstrToken;
+import assembly.Register;
+import assembly.TokenSequence;
+import assembly.tokens.PrintStringToken;
 
 /**
  * Class to represent print statements
@@ -14,5 +18,12 @@ public class PrintStat extends StatNode {
 	
 	public PrintStat(ExprNode expr) {
 		this.expr = expr;
+	}
+	
+	@Override
+	public TokenSequence toAssembly(Register register) {
+		InstrToken print = new PrintStringToken(expr.toString());
+		TokenSequence seq = new TokenSequence(print);
+		return seq;
 	}
 }
