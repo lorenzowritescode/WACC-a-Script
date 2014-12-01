822f5089581596456985908a2c95ce62ead48490
or813 <evenoli@dyn1205-147.wlan.ic.ac.uk>
2014-12-01 18:47:19 +0000

    Tidied up add token. removed addwith flag as unnecessary

--- src/compiler/assembly/tokens/AddToken.java
+++ src/compiler/assembly/tokens/AddToken.java
@@ -1,6 +1,5 @@
 package assembly.tokens;
 
-import tree.expr.IntLeaf;
 import assembly.InstrToken;
 import assembly.Register;
 
@@ -8,43 +7,22 @@ public class AddToken extends InstrToken{
 
 	private Register dest;
 	private Register op1;
-	private Object op2;
+	private Register op2;
 	private String condition = "";
 	
 	
-	public AddToken(Register dest, Register op1, IntLeaf op2) {
-		this(dest, op1);
-		this.op2 = op2;
-		this.addRegister(dest, op1);
-	}
-	
 	public AddToken(Register dest, Register op1, Register op2) {
-		this(dest, op1);
+		this.dest = dest;
+		this.op1 = op1;
 		this.op2 = op2;
 		this.addRegister(dest, op1, op2);
-	}
-	
-	public AddToken(String condition, Register dest, Register op1, IntLeaf op2) {
-		this(condition, dest, op1);
-		this.op2 = op2;
-		this.addRegister(dest, op1);
 	}
 	
 	public AddToken(String condition, Register dest, Register op1, Register op2) {
-		this(condition, dest, op1);
-		this.op2 = op2;
-		this.addRegister(dest, op1, op2);
-	}
-	
-	private AddToken(String condition, Register dest, Register op1) {
-		this.dest = dest;
+		this(dest, op1, op2);
 		this.condition = condition;
 	}
 	
-	private AddToken(Register dest, Register op1) {
-		this.dest = dest;
-		this.op1 = op1;
-	}
 	
 	@Override
 	public String toString() {
