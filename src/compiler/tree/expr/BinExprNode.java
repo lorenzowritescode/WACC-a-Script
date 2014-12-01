3c8d05bbece95f4e4e4707f6d0e30773940b6465
or813 <evenoli@dyn1205-147.wlan.ic.ac.uk>
2014-12-01 11:00:40 +0000

    Started work on the assembly methods of BinExprNode

--- src/compiler/tree/expr/BinExprNode.java
+++ src/compiler/tree/expr/BinExprNode.java
@@ -3,9 +3,15 @@ package tree.expr;
 import org.antlr.v4.runtime.ParserRuleContext;
 
 import symboltable.SymbolTable;
+import tree.type.WACCArithBinOp;
 import tree.type.WACCBinOp;
 import tree.type.WACCType;
 import WACCExceptions.InvalidTypeException;
+import assembly.InstrToken;
+import assembly.Register;
+import assembly.TokenSequence;
+import assembly.tokens.AddWithFlagToken;
+import assembly.tokens.OverflowToken;
 
 /* Represents a Binary Operator expression
  * Holds the operator and the expressions
@@ -47,6 +53,27 @@ public class BinExprNode extends ExprNode {
 				Math.max(lhs.weight() + 1, rhs.weight()),
 				Math.max(lhs.weight(), rhs.weight() + 1)
 		);
+	}
+
+	@Override
+	public TokenSequence printAssembly(Register r) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override 
+	public TokenSequence toAssembly(Register r) {
+		TokenSequence exprs = lhs.toAssembly(r);
+		exprs.appendAll(rhs.toAssembly(r.getNext()));
+		if (operator instanceof WACCArithBinOp) {
+			//TODO: this is hard...
+		}
+		
+		InstrToken add = new AddWithFlagToken("s", r, r, r.getNext());
+		InstrToken overflow = new OverflowToken();
+		exprs.append(add);
+		exprs.append(overflow);
+		return exprs;
 	}
 
 }
