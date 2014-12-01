3bf400c901528e03d5d8ccc172ed3ed77110836b
or813 <evenoli@dyn1205-147.wlan.ic.ac.uk>
2014-12-01 18:57:36 +0000

    Cleaned up Load Token, splitting it into two tokens

--- src/compiler/assembly/tokens/LoadToken.java
+++ src/compiler/assembly/tokens/LoadToken.java
@@ -9,59 +9,18 @@ public class LoadToken extends InstrToken {
 	private String source;
 	private String condition = "";
 
-//  Don't think we need this, leaving it just in case.	
-//	public LoadToken(Register dest, IntLeaf source) {
-//		this(dest);
-//		this.source = source.toString();
-//	}
-	
-//	public LoadToken(String condition, Register dest, IntLeaf source) {
-//		this(condition, dest);
-//		this.source = source.toString();
-//	}
 	
 	public LoadToken(Register dest, String label) {
-		this(dest);
+		this.dest = dest;
+		this.addRegister(dest);
 		this.source = "=" + label;
-	}
-	
-	public LoadToken(Register dest, Register source) {
-		this(dest);
-		this.source = "[" + source.toString() + "]";
-		this.addRegister(source);
-	}
-	
-	public LoadToken(Register dest, Register source, int offset) {
-		this(dest);
-		this.source = "[" + source.toString() + ", #" + offset + "]";
 	}
 	
 	public LoadToken(String condition, Register dest, String label) {
-		this(condition, dest);
-		this.source = "=" + label;
-	}
-	
-	public LoadToken(String condition, Register dest, Register source) {
-		this(condition, dest);
-		this.source = "[" + source.toString() + "]";
-		this.addRegister(source);
-	}
-	
-	public LoadToken(String condition, Register dest, Register source, int offset) {
-		this(dest);
-		this.source = "[" + source.toString() + ", #" + offset + "]";
-		this.addRegister(source);
-	}
-	
-	private LoadToken(Register dest) {
-		this.dest = dest;
-		this.addRegister(dest);
-	}
-	private LoadToken(String condition, Register dest) {
-		this(dest);
+		this(dest, label);
 		this.condition = condition;
-		this.addRegister(dest);
 	}
+	
 
 	@Override 
 	public String toString() {
