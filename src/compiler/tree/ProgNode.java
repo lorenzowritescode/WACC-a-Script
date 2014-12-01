8eca539ba695f182c46ff48cc0303d5befad47b2
or813 <evenoli@dyn1205-147.wlan.ic.ac.uk>
2014-12-01 19:38:55 +0000

    Implemented toAssembly for ProgNode

--- src/compiler/tree/ProgNode.java
+++ src/compiler/tree/ProgNode.java
@@ -8,6 +8,12 @@ import symboltable.SymbolTable;
 import tree.func.FuncDecNode;
 import tree.stat.StatNode;
 import tree.type.WACCType;
+import assembly.Register;
+import assembly.TokenSequence;
+import assembly.tokens.LabelToken;
+import assembly.tokens.MovImmToken;
+import assembly.tokens.PopToken;
+import assembly.tokens.PushToken;
 
 public class ProgNode extends WACCTree {
 	
@@ -27,6 +33,30 @@ public class ProgNode extends WACCTree {
 	@Override
 	public WACCType getType() {
 		throw new UnsupportedOperationException("getType() is not supported in ProgNode");
+	}
+	
+	@Override
+	public TokenSequence toAssembly(Register r) {
+		TokenSequence seq = new TokenSequence();
+		
+		//Create main label and push lr
+		seq.append(new LabelToken("main"));
+		seq.append(new PushToken(Register.lr));
+		
+		//Get assembly of main function
+		seq.appendAll(progBody.toAssembly(r));
+		
+		//Get assembly of functions
+		for(FuncDecNode func : functions) {
+			seq.appendAll(func.toAssembly(r));
+		}
+		
+		//return 0 and Pop pc
+		seq.append(new MovImmToken(Register.R0, "#0"));
+		seq.append(new PopToken(Register.pc));
+		
+		
+		return seq;
 	}
 
 }
