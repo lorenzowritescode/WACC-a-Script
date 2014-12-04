8e9f8bd0bc69f6eac35e2953b56af933409c9dd6
or813 <evenoli@dyn1205-147.wlan.ic.ac.uk>
2014-12-04 15:33:14 +0000

    Added StorePreIndexToken

---
+++ src/compiler/assembly/tokens/StorePreIndexToken.java
@@ -0,0 +1,29 @@
+package assembly.tokens;
+
+import assembly.Register;
+
+public class StorePreIndexToken extends StoreToken {
+
+	public StorePreIndexToken(Register source, Register destAddress) {
+		super(source, destAddress);
+	}
+	public StorePreIndexToken(Register source, Register destAddress, int offset) {
+		super(source, destAddress, offset);
+	}
+	
+	public StorePreIndexToken(String condition, Register source, Register destAddress) {
+		super(condition, source, destAddress);
+	}
+	
+	public StorePreIndexToken(String condition, Register source, Register destAddress, int offset) {
+		super(condition, source, destAddress, offset);
+	}
+	
+	@Override 
+	public String toString() {
+		return (offsetSet && offset != 0) ?
+				"STR" + condition +  " " + source.toString() + ", " + "[" + dest.toString() + ", #" + offset + "]!" :
+					"STR" + condition +  " " + source.toString() + ", " + "[" + dest.toString() + "]!";
+	}
+
+}
