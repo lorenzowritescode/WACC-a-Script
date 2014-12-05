804a03a878fabd6d8da763a0458ef0803a5d1e23
or813 <evenoli@dyn1205-147.wlan.ic.ac.uk>
2014-12-05 13:59:31 +0000

    Added chack bounds token

--- src/compiler/assembly/system/SystemErrorTokens.java
+++ src/compiler/assembly/system/SystemErrorTokens.java
@@ -5,6 +5,34 @@ import assembly.TokenSequence;
 
 public class SystemErrorTokens {
 
+	public static final InstrToken CHECK_BOUNDS = new InstrToken() {
+		
+		public TokenSequence toPrepend() {
+			return new TokenSequence(
+					SystemErrorTokens.OUT_OF_BOUNDS_MESSAGE,
+					SystemFormatterTokens.STRING_FORMATTER);
+		}
+		
+		@Override
+		public String toString() {
+			return "p_check_array_bounds:"
+						+ "\n\tPUSH {lr}"
+						+ "\n\tCMP r0, #0"
+						+ "\n\tLDRLT r0, =msg_10"
+						+ "\n\tBLLT p_throw_runtime_error"
+						+ "\n\tLDR r1, [r1]"
+						+ "\n\tCMP r0, r1"
+						+ "\n\tLDRCS r0, =msg_11"
+						+ "\n\tBLCS p_throw_runtime_error"
+						+ "\n\tPOP {pc}";
+		}
+		
+		@Override
+		public boolean requiresTab() {
+			return false;
+		}
+	};
+
 	public static InstrToken OVERFLOW_ERROR = new InstrToken() {
 
 		public TokenSequence toPrepend() {
@@ -75,6 +103,11 @@ public class SystemErrorTokens {
 					+ "\n\t.ascii \"NullReferenceError: "
 					+ "dereference a null reference\\n\\0\"";
 		}
+		
+		@Override
+		public boolean requiresTab() {
+			return false;
+		}
 	};
 	
 	public static InstrToken DIVIDE_BY_ZERO_MESSAGE = new InstrToken() {
@@ -98,6 +131,23 @@ public class SystemErrorTokens {
 					+ "\n\t.word 82"
 					+ "\n\t.ascii	\"OverflowError: the result is too small/large "
 					+ "to store in a 4-byte signed-integer.\\n\"";
+		}
+		
+		@Override
+		public boolean requiresTab() {
+			return false;
+		}
+	};
+	
+	public static final InstrToken OUT_OF_BOUNDS_MESSAGE = new InstrToken() {
+		@Override
+		public String toString() {
+			return "msg_10:"
+					+ "\n\t.word 44"
+					+ "\n\t.ascii	\"ArrayIndexOutOfBoundsError: negative index\\n\\0\""
+					+"msg_11:"
+					+ "\n\t.word 45"
+					+ "\n\t.ascii	\"ArrayIndexOutOfBoundsError: index too large\\n\\0\"";
 		}
 		
 		@Override
