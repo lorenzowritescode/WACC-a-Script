1776eaede07c1b9c92d0d5a755b53181376253ed
or813 <evenoli@dyn1205-147.wlan.ic.ac.uk>
2014-12-06 15:36:35 +0000

    Fixed syntax error in add token

--- src/compiler/assembly/tokens/AddToken.java
+++ src/compiler/assembly/tokens/AddToken.java
@@ -10,6 +10,7 @@ public class AddToken extends InstrToken{
 	private Register op2;
 	private String condition = "";
 	private String shift = "";
+	private boolean shiftSet = false;
 	
 	
 	public AddToken(Register dest, Register op1, Register op2) {
@@ -27,11 +28,14 @@ public class AddToken extends InstrToken{
 	public AddToken(Register dest, Register op1, Register op2, String shift) {
 		this(dest, op1, op2);
 		this.shift = shift;
+		shiftSet = true;
 	}
 	
 	@Override
 	public String toString() {
-		return "ADD" + condition + " " + dest.toString() + ", " + op1.toString() + ", " + op2.toString() + ", " + shift;
+		return shiftSet?
+				"ADD" + condition + " " + dest.toString() + ", " + op1.toString() + ", " + op2.toString() + ", " + shift:
+					"ADD" + condition + " " + dest.toString() + ", " + op1.toString() + ", " + op2.toString();
 	}
 	
 }
