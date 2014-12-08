73c6f8f7a1f32d77323ec38de408a66b3eb20ff0
or813 <evenoli@dyn1205-147.wlan.ic.ac.uk>
2014-12-08 13:23:58 +0000

    Fixed bug where return would not pop PC

--- src/compiler/tree/stat/ReturnStatNode.java
+++ src/compiler/tree/stat/ReturnStatNode.java
@@ -6,9 +6,12 @@ import symboltable.SymbolTable;
 import tree.expr.ExprNode;
 import tree.type.WACCType;
 import WACCExceptions.IncompatibleTypesException;
+import assembly.InstrToken;
 import assembly.Register;
+import assembly.StackAllocator;
 import assembly.TokenSequence;
 import assembly.tokens.MovRegToken;
+import assembly.tokens.PopToken;
 
 /**
  * Class to represent return statements for functions
@@ -36,7 +39,11 @@ public class ReturnStatNode extends StatNode {
 	public TokenSequence toAssembly(Register r) {
 		TokenSequence seq = new TokenSequence(expr.toAssembly(r));
 		seq.append(new MovRegToken(Register.R0, r));
-		return seq;
+		
+		TokenSequence stackTerminationSeq = StackAllocator.stackAllocator.getTermination();
+		stackTerminationSeq.append(new PopToken(Register.pc));
+		
+		return seq.appendAll(stackTerminationSeq);
 	}
 	
 }
