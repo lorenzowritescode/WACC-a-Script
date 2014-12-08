552214b5e290187b649a49c1c689338066d9e8a2
or813 <evenoli@dyn1205-147.wlan.ic.ac.uk>
2014-12-08 17:53:47 +0000

    Removed unused constructor

--- src/compiler/assembly/tokens/AndToken.java
+++ src/compiler/assembly/tokens/AndToken.java
@@ -16,15 +16,6 @@ public class AndToken extends InstrToken {
 		this.addRegister(dest, op1, op2);
 	}
 	
-	//TODO: Should this take bool leaf? or more general object? 
-	//Spec says AND can take immediate value, but can it be anything else? 
-	public AndToken(Register dest, Register op1, Object op2){
-		this.dest = dest;
-		this.op1 = op1;
-		this.op2 = op2;
-		this.addRegister(dest, op1);
-	}
-	
 	@Override
 	public String toString() {
 		return "AND " + dest.toString() + ", " + op1.toString() + ", " + op2.toString();
