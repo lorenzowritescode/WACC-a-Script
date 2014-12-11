93fc593a659c8778533751c3b60aaa441274f9fa
or813 <evenoli@dyn1205-147.wlan.ic.ac.uk>
2014-12-11 19:19:41 +0000

    Added JSAssign and getters for AssignStatNode

---
+++ extension/JSTree/JSAssignStat.java
@@ -0,0 +1,19 @@
+package JSTree;
+
+public class JSAssignStat implements JSTree {
+
+	private JSTree lhs;
+	private JSTree rhs;
+
+	public JSAssignStat(JSTree lhs, JSTree rhs) {
+		this.lhs = lhs;
+		this.rhs = rhs;
+	}
+
+	@Override
+	public String toCode() {
+		return lhs.toCode() + " = " + rhs.toCode();
+	}
+	
+	
+}
