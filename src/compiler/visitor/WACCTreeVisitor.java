91ffb9524a080dd7e68e0f4120ddc3ead9b193c7
Sam Budd <samuel.budd13@imperial.ac.uk>
2014-12-11 15:23:08 +0000

    Assignments

--- src/compiler/visitor/WACCTreeVisitor.java
+++ src/compiler/visitor/WACCTreeVisitor.java
@@ -1,6 +1,18 @@
 package visitor;
 
-import tree.expr.*;
+import tree.assignments.ArgListNode;
+import tree.assignments.ArrayElemNode;
+import tree.assignments.ArrayLiterNode;
+import tree.assignments.CallStatNode;
+import tree.assignments.NewPairNode;
+import tree.assignments.PairElemNode;
+import tree.expr.BinExprNode;
+import tree.expr.BoolLeaf;
+import tree.expr.CharLeaf;
+import tree.expr.IntLeaf;
+import tree.expr.PairLeaf;
+import tree.expr.PairLiterNode;
+import tree.expr.StringLeaf;
 
 public abstract class WACCTreeVisitor<T> {
 	public abstract T visitCharLeaf(CharLeaf node);
@@ -11,5 +23,11 @@ public abstract class WACCTreeVisitor<T> {
 	public abstract T visitPairLiterNode(PairLiterNode node);
 	public abstract T visitStringLeaf(StringLeaf node);
 	
-	
+	//Assignment
+	public abstract T visitArgListNode(ArgListNode node);
+	public abstract T visitArrayElemNode(ArrayElemNode node);
+	public abstract T visitArrayLiterNode(ArrayLiterNode node);
+	public abstract T visitCallStatNode(CallStatNode node);
+	public abstract T visitNewPairNode(NewPairNode node);
+	public abstract T visitPairElemNode(PairElemNode node);
 }
