b5eb93b5de531589250cfd781736c1d37a307b1c
Lorenzo Paoliani <lorenzo.paoliani13@imperial.ac.uk>
2014-12-11 15:06:35 +0000

    More werk

--- src/compiler/visitor/WACCTreeVisitor.java
+++ src/compiler/visitor/WACCTreeVisitor.java
@@ -6,8 +6,10 @@ public abstract class WACCTreeVisitor<T> {
 	public abstract T visitCharLeaf(CharLeaf node);
 	public abstract T visitBoolLeaf(BoolLeaf node);
 	public abstract T visitBinExprNode(BinExprNode node);
-	public abstract T visitCharLeaf(CharLeaf node);
-	public abstract T visitCharLeaf(CharLeaf node);
-	public abstract T visitCharLeaf(CharLeaf node);
+	public abstract T visitIntLeaf(IntLeaf node);
+	public abstract T visitPairLeaf(PairLeaf node);
+	public abstract T visitPairLiterNode(PairLiterNode node);
+	public abstract T visitStringLeaf(StringLeaf node);
+	
 	
 }
