33800c686d552d42b760300c5183347704689e53
Lorenzo Paoliani <lorenzo.paoliani13@imperial.ac.uk>
2014-12-11 15:18:32 +0000

    added func visit functions

--- src/compiler/visitor/WACCTreeVisitor.java
+++ src/compiler/visitor/WACCTreeVisitor.java
@@ -1,15 +1,24 @@
 package visitor;
 
 import tree.expr.*;
+import tree.func.FuncDecNode;
+import tree.func.ParamListNode;
+import tree.func.ParamNode;
 
 public abstract class WACCTreeVisitor<T> {
+	// Expr
+	public abstract T visitBinExprNode(BinExprNode node);
 	public abstract T visitCharLeaf(CharLeaf node);
 	public abstract T visitBoolLeaf(BoolLeaf node);
-	public abstract T visitBinExprNode(BinExprNode node);
 	public abstract T visitIntLeaf(IntLeaf node);
 	public abstract T visitPairLeaf(PairLeaf node);
 	public abstract T visitPairLiterNode(PairLiterNode node);
 	public abstract T visitStringLeaf(StringLeaf node);
+	public abstract T visitUnExprNode(UnExprNode node);
+	public abstract T visitVarNode(VarNode node);
 	
-	
+	// Func
+	public abstract T visitFuncDecNode(FuncDecNode node);
+	public abstract T visitParamListNode(ParamListNode node);
+	public abstract T visitParamNode(ParamNode node);
 }
