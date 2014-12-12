1f2adaf71ec8d340342b9b80d37f626a365a2a40
or813 <evenoli@dyn1205-147.wlan.ic.ac.uk>
2014-12-12 12:06:49 +0000

    Removed extra set of brackets in function definitions

--- extension/JSTree/func/JSFunc.java
+++ extension/JSTree/func/JSFunc.java
@@ -17,7 +17,7 @@ public class JSFunc implements JSTree {
 
 	@Override
 	public String toCode() {
-		return "function " + functionName + "(" + params.toCode() + ") {"
+		return "function " + functionName + params.toCode() + " {"
 				+ functionBody.toCode()
 				+ "}";
 	}
