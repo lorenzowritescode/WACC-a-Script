36c1da338117fb1a51cb198602f3a5c0fd3e3fd0
Lorenzo Paoliani <lorenzo.paoliani13@imperial.ac.uk>
2014-12-12 23:11:04 +0000

    Added stub math lib and changes to core.js

--- src/extension/js-lib/core.js
+++ src/extension/js-lib/core.js
@@ -1,12 +1,13 @@
-function Pair(x, y) {
+/* @flow */
+function Pair(x :any, y :any) :Object{
 	return {fst: x, snd: y};
 }
 
-function print(s) {
+function print(s :string) :void {
 	console.log(s);
 }
 
-function read(callback) {
+function read() {
 	var readline = require('readline'),
 	rl = readline.createInterface({
 		input: process.stdin,
@@ -15,4 +16,12 @@ function read(callback) {
 
 	var res;
 	rl.question("", callback);
+
+	var uvrun = require("uvrun");
+	while (!res)
+	uvrun.runOnce();
+	return res;
 }
+
+x = read();
+console.log(x);
