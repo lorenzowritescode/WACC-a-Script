7be22fa49c9b145c171225fc97de06f8200c672c
Lorenzo Paoliani <lorenzo.paoliani13@imperial.ac.uk>
2014-12-15 17:10:13 +0000

    Implemented visitor

--- src/extension/jsparser/JSFlowTypeVisitor.java
+++ src/extension/jsparser/JSFlowTypeVisitor.java
@@ -1,10 +1,14 @@
 package jsparser;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jsparser.build.JSTypeParser.ArglistContext;
 import jsparser.build.JSTypeParser.ArgumentContext;
 import jsparser.build.JSTypeParser.FunctionContext;
 import jsparser.build.JSTypeParser.LibraryContext;
 import jsparser.build.JSTypeParser.TypeContext;
+import jsparser.build.JSTypeParser.TypeDefContext;
 import jsparser.build.JSTypeParserBaseVisitor;
 
 public class JSFlowTypeVisitor extends JSTypeParserBaseVisitor<Object> {
@@ -16,26 +20,32 @@ public class JSFlowTypeVisitor extends JSTypeParserBaseVisitor<Object> {
 
 	@Override
 	public Object visitLibrary(LibraryContext ctx) {
-		// TODO Auto-generated method stub
-		return super.visitLibrary(ctx);
+		List<LibFunc> library = new ArrayList<>();
+		for (FunctionContext f:ctx.function()) {
+			LibFunc lf = (LibFunc) visit(f);
+			library.add(lf);
+		}
+		return library;
 	}
 
 	@Override
 	public Object visitFunction(FunctionContext ctx) {
-		// TODO Auto-generated method stub
-		return super.visitFunction(ctx);
+		String fName = ctx.IDENTITY().getText();
+		LibType returnType = (LibType) visit(ctx.typeDef());
+		LibArgList args = (LibArgList) visit(ctx.arglist());
+		
+		return new LibFunc(fName, returnType, args);
+	}
+
+	@Override
+	public Object visitTypeDef(TypeDefContext ctx) {
+		return LibType.parse(ctx.type().getText());
 	}
 
 	@Override
 	public Object visitArglist(ArglistContext ctx) {
 		// TODO Auto-generated method stub
 		return super.visitArglist(ctx);
-	}
-
-	@Override
-	public Object visitType(TypeContext ctx) {
-		// TODO Auto-generated method stub
-		return super.visitType(ctx);
 	}
 
 }
