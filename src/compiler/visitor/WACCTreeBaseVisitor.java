73e353f2380ee59b4321565df4ef1dbf881a395f
or813 <evenoli@dyn1205-147.wlan.ic.ac.uk>
2014-12-16 19:08:20 +0000

    Removed todos

--- src/compiler/visitor/WACCTreeBaseVisitor.java
+++ src/compiler/visitor/WACCTreeBaseVisitor.java
@@ -38,199 +38,166 @@ public class WACCTreeBaseVisitor<T> extends WACCTreeVisitor<T> {
 
 	@Override
 	public T visit(WACCTree node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitProgNode(ProgNode node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitBinExprNode(BinExprNode node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitCharLeaf(CharLeaf node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitBoolLeaf(BoolLeaf node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitIntLeaf(IntLeaf node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitPairLeaf(PairLeaf node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitPairLiterNode(PairLiterNode node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitStringLeaf(StringLeaf node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitUnExprNode(UnExprNode node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitVarNode(VarNode node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitFuncDecNode(FuncDecNode node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitParamListNode(ParamListNode node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitParamNode(ParamNode node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitArgListNode(ArgListNode node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitArrayElemNode(ArrayElemNode node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitArrayLiterNode(ArrayLiterNode node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitCallStatNode(CallStatNode node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitNewPairNode(NewPairNode node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitPairElemNode(PairElemNode node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitAssignStatNode(AssignStatNode node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitBlockStatNode(BlockStatNode node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitExitStat(ExitStat node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitFreeStat(FreeStat node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitIfStatNode(IfStatNode node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitPrintLnStat(PrintLnStat node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitPrintStat(PrintStat node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitReadStatNode(ReadStatNode node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitReturnStatNode(ReturnStatNode node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitSeqStatNode(SeqStatNode node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitSkipStatNode(SkipStatNode node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitVarDecNode(VarDecNode node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T visitWhileStatNode(WhileStatNode node) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
