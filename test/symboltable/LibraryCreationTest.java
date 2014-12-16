794c67e01789e412b058e48a1428befad7410a50
or813 <evenoli@dyn1205-147.wlan.ic.ac.uk>
2014-12-16 15:24:56 +0000

    Updated library creation test

--- test/symboltable/LibraryCreationTest.java
+++ test/symboltable/LibraryCreationTest.java
@@ -1,22 +1,25 @@
 package symboltable;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import jsparser.LibArg;
+import jsparser.LibArgList;
+import jsparser.LibFunc;
+import jsparser.LibraryLoader;
+import jsparser.types.LibBaseType;
 
 import org.junit.Test;
 
 import tree.type.WACCType;
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-import jsparser.LibArg;
-import jsparser.LibArgList;
-import jsparser.LibFunc;
-import jsparser.LibType;
 
 public class LibraryCreationTest {
-	LibArg arg = new LibArg(LibType.NUMBER);
+	LibArg arg = new LibArg(LibBaseType.NUMBER);
 	LibArgList l = new LibArgList(arg);
-	LibFunc func = new LibFunc("FunctionYay!",LibType.CHAR, l);
+	LibFunc func = new LibFunc("FunctionYay!",LibBaseType.CHAR, l);
 	List<LibFunc> lib = new ArrayList<LibFunc>();
 	boolean b = lib.add(func);
 	
@@ -24,8 +27,9 @@ public class LibraryCreationTest {
 	
 	@Test
 	public void symbolTablesConstructedWithExternalLibrariesContainTheCorrectObjects() {
-		
-		SymbolTable st = new SymbolTable(lib);
+		SymbolTable st = new SymbolTable();
+		LibraryLoader ll = new LibraryLoader(st);
+		ll.loadIntoSymbolTable(lib);
 		assertThat(st.containsRecursive("FunctionYay!"), is(true));
 		assertThat(st.get("FunctionYay!").getType(), is(WACCType.CHAR));
 		
