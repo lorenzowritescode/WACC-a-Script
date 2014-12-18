93f001db75ee80bc6a7639e52e0168a08bf667b3
Lorenzo Paoliani <lorenzo.paoliani13@imperial.ac.uk>
2014-12-18 21:19:48 +0100

    I pressed a key and something happened apparently

--- src/main/Main.java
+++ src/main/Main.java
@@ -36,35 +36,36 @@ import WACCExceptions.IntOverflowException;
 import WACCExceptions.UnresolvedExpectationException;
 
 public class Main {
-	
+
 	public static void main(String[] args) throws IOException {
 		// Parse the flags given in the command line arguments
 		CommandLine cmd = parseFlags(args);
-		
+
 		// We set the logging options as appropriate
 		SemanticChecker.dbh.setOptions(cmd);
-		
+
 		// Get WACC source File
 		String waccFilePath = cmd.getOptionValue("f");
 		InputStream waccInput = getInputStream(waccFilePath);
-		
+
 		WACCLexer lexer = getLexer(waccInput);
-		
+
 		// Tokenise the input stream
 		CommonTokenStream tokens = tokenise(lexer);
-		
+
 		// Build the tokens AST
 		ParseTree tree = getParseTree(tokens);
-		
+
 		// Get external JS source file
 		SymbolTable libTable = new SymbolTable();
 		LibraryLoader ll = new LibraryLoader(libTable);
-		
-		// funcDependancies stores the function name, and the required file name for that function
+
+		// funcDependancies stores the function name, and the required file name
+		// for that function
 		// (e.g. <Function Name, Required Package name>)
 		HashMap<String, String> funcDependancies = new HashMap<>();
-		
-		if(cmd.hasOption("j") && cmd.hasOption("l")) {
+
+		if (cmd.hasOption("j") && cmd.hasOption("l")) {
 			String[] libs = cmd.getOptionValue("l").split(":");
 			for (String lib : libs) {
 				JSLibParser libParser = new JSLibParser(lib);
@@ -73,22 +74,21 @@ public class Main {
 				funcDependancies.putAll(ll.findDependancies(lib, funcs));
 			}
 		}
-		
+
 		// Check for semantic errors
 		SemanticChecker sc = checkSemanticIntegrity(tree, libTable);
-		
+
 		// Check that the Error Listener has not recorded any exceptions
-		if ( !sc.terminate() ) {
+		if (!sc.terminate()) {
 			exitSemanticFailure();
 		}
-		
+
 		// Extract the path from the waccFilePath string
 		if (waccFilePath == null) {
 			waccFilePath = "temp.wacc";
 		}
-			
+
 		Path p = Paths.get(waccFilePath);
-		 
 
 		// String that will contain the final program
 		String outputString = "";
@@ -96,18 +96,19 @@ public class Main {
 		String extension = "";
 		// WACCTree that contains the program
 		WACCTree wt = sc.getProgTree();
-		
+
 		// Get the filename and replace the extension
-		if(cmd.hasOption('j')) {
+		if (cmd.hasOption('j')) {
 			outputString = compileToJavaScript(wt, funcDependancies);
 			extension = ".js";
 		} else {
 			outputString = compileToArmAssembly(wt);
 			extension = ".s";
 		}
-		
-		String outputFileName = p.getFileName().toString().replace(".wacc", extension);
-		
+
+		String outputFileName = p.getFileName().toString()
+				.replace(".wacc", extension);
+
 		// If the s flag is on we want to print to stdout;
 		if (cmd.hasOption("s")) {
 			System.out.println(outputString);
@@ -116,24 +117,28 @@ public class Main {
 		}
 	}
 
-
-
-	private static String compileToJavaScript(WACCTree wt, HashMap<String, String> funcDeps) {
+	private static String compileToJavaScript(WACCTree wt,
+			HashMap<String, String> funcDeps) {
 		WACCTreeToJsTree converter = new WACCTreeToJsTree(wt, funcDeps);
-		return converter.init();	
+		return converter.init();
 	}
 
 	private static String compileToArmAssembly(WACCTree wt) {
 		WACCCompiler compiler = new WACCCompiler(wt);
 		compiler.init();
 		String compilerOutput = compiler.toString();
-		
+
 		return compilerOutput;
 	}
 
-	/** Utility method to extract the assembly filename and write to file
-	 * @param resultString A String containing the assembly output
-	 * @param fileName The String containing the path of the source file. It works with just the filename.
+	/**
+	 * Utility method to extract the assembly filename and write to file
+	 * 
+	 * @param resultString
+	 *            A String containing the assembly output
+	 * @param fileName
+	 *            The String containing the path of the source file. It works
+	 *            with just the filename.
 	 */
 	private static void writeToFile(String resultString, String fileName) {
 		// Write to file
@@ -142,19 +147,18 @@ public class Main {
 			BufferedWriter output = new BufferedWriter(new FileWriter(file));
 			output.write(resultString);
 			output.close();
-		} catch ( IOException e ) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
 	/**
 	 * @param waccInput
-	 * 		The InputStream containing the WACC program
-	 * @return
-	 * 		A WACCLexer
+	 *            The InputStream containing the WACC program
+	 * @return A WACCLexer
 	 * @throws IOException
 	 */
-	
+
 	private static WACCLexer getLexer(InputStream waccInput) throws IOException {
 		// Create ANTLR Input stream
 		WACCLexer lexer = null;
@@ -169,13 +173,12 @@ public class Main {
 		}
 		return lexer;
 	}
-	
+
 	private static CommonTokenStream tokenise(WACCLexer lexer) {
 		CommonTokenStream tokens = null;
 		try {
 			tokens = new CommonTokenStream(lexer);
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			System.err.println(e.toString());
 			exitSemanticFailure();
 		}
@@ -184,11 +187,11 @@ public class Main {
 
 	/**
 	 * @param tree
-	 * 		An ANTLR.ParseTree that represents a WACC program
-	 * @return
-	 * 		The Semantic Checker object
+	 *            An ANTLR.ParseTree that represents a WACC program
+	 * @return The Semantic Checker object
 	 */
-	private static SemanticChecker checkSemanticIntegrity(ParseTree tree, SymbolTable libTable) {
+	private static SemanticChecker checkSemanticIntegrity(ParseTree tree,
+			SymbolTable libTable) {
 		SemanticChecker semantic = new SemanticChecker(tree, libTable);
 		try {
 			semantic.init();
@@ -207,9 +210,8 @@ public class Main {
 
 	/**
 	 * @param tokens
-	 * 		The tokens of the WACC program
-	 * @return
-	 * 		An ANTLR AST representing the program
+	 *            The tokens of the WACC program
+	 * @return An ANTLR AST representing the program
 	 */
 	private static ParseTree getParseTree(CommonTokenStream tokens) {
 		WACCParser parser = null;
@@ -223,8 +225,8 @@ public class Main {
 			System.err.println(e.toString());
 			exitSyntaxError();
 		}
-		
-		if(parser.getNumberOfSyntaxErrors() > 0) {
+
+		if (parser.getNumberOfSyntaxErrors() > 0) {
 			exitSyntaxError();
 		}
 		return tree;
@@ -232,17 +234,16 @@ public class Main {
 
 	/**
 	 * @param waccFilePath
-	 * 		The path of the File to be compiled
-	 * @return
-	 * 		The InputStream 
+	 *            The path of the File to be compiled
+	 * @return The InputStream
 	 * @throws FileNotFoundException
-	 * 		If the filePath provided is not found.
+	 *             If the filePath provided is not found.
 	 */
 	private static InputStream getInputStream(String waccFilePath)
 			throws FileNotFoundException {
 		// Initialise InputStream to the file or System.in if null.
 		InputStream waccInput;
-		if(waccFilePath != null)
+		if (waccFilePath != null)
 			waccInput = new FileInputStream(waccFilePath);
 		else
 			waccInput = System.in;
@@ -251,33 +252,33 @@ public class Main {
 
 	/**
 	 * @param args
-	 * 		The arguments array passed to the command line
-	 * @return
-	 * 		A CommandLine object that contains all the flag values.
+	 *            The arguments array passed to the command line
+	 * @return A CommandLine object that contains all the flag values.
 	 */
 	private static CommandLine parseFlags(String[] args) {
 		// create Options object
 		Options options = new Options();
-		// The first argument is the flag character, the second argument 
-		// is true if the flag requires and argument. The third flag describes the behavior of the flag
+		// The first argument is the flag character, the second argument
+		// is true if the flag requires and argument. The third flag describes
+		// the behavior of the flag
 		options.addOption("v", false, "verbose");
 		options.addOption("d", false, "debug mode");
 		options.addOption("f", true, "source file");
 		options.addOption("s", false, "force printing assembly to std-out");
 		options.addOption("j", false, "Compile into Javascript");
-		options.addOption("l", true, "Include external javaScript libraries (e.g. \"core.js:math.js\"");
-		
+		options.addOption("l", true,
+				"Include external javaScript libraries (e.g. \"core.js:math.js\"");
+
 		CommandLineParser flagsParser = new PosixParser();
 		CommandLine cmd = null;
 		try {
-			 cmd = flagsParser.parse(options, args);
+			cmd = flagsParser.parse(options, args);
 		} catch (ParseException e) {
 			System.err.println("There were problems parsing the flags");
 			System.err.println(e.toString());
 		}
 		return cmd;
 	}
-	
 
 	private static void exitSyntaxError() {
 		System.exit(100);
