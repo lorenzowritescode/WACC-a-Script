d9b936eb52e624148734806d0ef9be920afd65f9
Lorenzo Paoliani <lorenzo.paoliani13@imperial.ac.uk>
2015-01-04 11:49:25 +0000

    Added an option for no-core

--- src/extension/JSTree/JSProg.java
+++ src/extension/JSTree/JSProg.java
@@ -16,8 +16,9 @@ public class JSProg extends JSTree {
 	private JSStat body;
 	private HashMap<String, String> funcDeps;
 	private String corePath;
-	
-	public JSProg(List<JSFunc> functions, JSStat body, HashMap<String, String> funcDeps) {
+
+	public JSProg(List<JSFunc> functions, JSStat body,
+			HashMap<String, String> funcDeps) {
 		this.functions = functions;
 		this.body = body;
 		this.funcDeps = funcDeps;
@@ -27,48 +28,53 @@ public class JSProg extends JSTree {
 	public String toCode() {
 		// Loading dependencies
 		Set<String> set = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
-        set.addAll(funcDeps.values());
-        ArrayList<String> deps = new ArrayList<>(set);
+		set.addAll(funcDeps.values());
+		ArrayList<String> deps = new ArrayList<>(set);
 		String dependencies = "";
-		
-		for(String filePath : deps) {
+
+		for (String filePath : deps) {
 			String fileName = new File(filePath).getName();
 			fileName = fileName.substring(0, fileName.lastIndexOf('.'));
-			
+
 			String rawFilePath = filePath.replace("js-lib", "raw-js-lib");
-			dependencies += "var " + fileName + " = require('./" + rawFilePath + "');\n";
+			dependencies += "var " + fileName + " = require('./" + rawFilePath
+					+ "');\n";
 		}
-		
+
 		String functionDecs = "";
-		for (JSFunc f:functions) {
+		for (JSFunc f : functions) {
 			functionDecs += "\n" + f.toCode();
 		}
-		
+
 		String bodyString = body.toCode();
-		
+
 		bodyString += "\ncore.terminate();\n";
-		for(int i = 0; i < body.depthIncremented(); i++) {
+		for (int i = 0; i < body.depthIncremented(); i++) {
 			bodyString += "})\n";
 		}
-		
+
 		bodyString += functionDecs;
-		
-		return dependencies + wrapMainBody(bodyString, !corePath.equalsIgnoreCase("no_core"));
+
+		return dependencies
+				+ wrapMainBody(bodyString,
+						!corePath.equalsIgnoreCase("no_core"));
 	}
-	
+
 	private String wrapMainBody(String body, boolean requiresCore) {
+		// If the core will be embedded externally
 		String coreDec = "";
 		String functionDec = "function program (core) {\n";
 		String functionEnd = "}\n";
 		String moduleExport = "module.exports = { program: program }\n";
 		
+		// If the core is to be embedded now
 		if (requiresCore) {
 			coreDec = "var core = require('" + corePath + "');\n";
 			functionDec = "(function() {\n";
 			functionEnd = "})();";
 			moduleExport = "";
 		}
-		
+
 		return coreDec + functionDec + body + functionEnd + moduleExport;
 	}
 
